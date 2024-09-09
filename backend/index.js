6c2f331f47a779246e14d0a74e15c06062372909
dinantaadhikary <dinanta_acse2022@msit.edu.in>
2024-09-09 16:05:31 +0530

    index created

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("Listening"); 
+})

a8f62a2a27cb60523afd1e534d19d6ff62d1259e
dinantaadhikary <dinanta_acse2022@msit.edu.in>
2024-09-11 00:41:34 +0530

    nodemon dotenv added

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
+import dotenv from "dotenv"
 import express from "express"
-
+dotenv.config()
+const port=4000
 const app = express()
 
+
 app.get("/",(req,res)=>{
-    res.send("hello world")
+    res.send("Danish Zehen")
 })
 
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log("Listening"); 
+app.get("/insta",(req,res)=>{
+    res.send('<h2>chud gaye guru</h2>')
+}
+)
+
+app.listen(process.env.PORT || port, ()=>{
+    console.log("listening"); 
 })

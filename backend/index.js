70c17a466cce6cfd20bfde61faebc5071a4d7889
dinantaadhikary <dinanta_acse2022@msit.edu.in>
2024-09-11 16:24:01 +0530

    frontend written

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,36 @@ app.get("/insta",(req,res)=>{
     res.send('<h2>chud gaye guru</h2>')
 }
 )
+app.get('/api/jokes',(req,res)=>{
+    const joke=[
+        {
+          "id": 1,
+          "title": "Why don't scientists trust atoms?",
+          "content": "Because they make up everything!"
+        },
+        {
+          "id": 2,
+          "title": "What do you call fake spaghetti?",
+          "content": "An impasta!"
+        },
+        {
+          "id": 3,
+          "title": "Why did the scarecrow win an award?",
+          "content": "Because he was outstanding in his field!"
+        },
+        {
+          "id": 4,
+          "title": "Why don't skeletons fight each other?",
+          "content": "They don't have the guts."
+        },
+        {
+          "id": 5,
+          "title": "What do you call cheese that isn't yours?",
+          "content": "Nacho cheese!"
+        },
+      ]
+     res.send(joke)      
+})
 
 app.listen(process.env.PORT || port, ()=>{
     console.log("listening"); 
